549a30b15d292648f720a2e9dfdf150a9b92c2d9
dino <dino.decastro@gmail.com>
2019-02-24 19:59:40 +0800

    Cleaned book 3

--- static/quotes/book_03.py
+++ static/quotes/book_03.py
@@ -8,8 +8,7 @@ quotes_list = [
     You boarded, you set sail, you've made the passage. Time to disembark. If
     it's for another life, well, there's nowhere without gods on that side
     either. If to nothingness, then you no longer have to put up with pain and
-    pleasure, or go on dancing attendance on this battered crate, your body -
-    so much inferior to that which serves it.
+    pleasure.
     """,
     """
     Don't waste the rest of your time here worrying about other people - unless
@@ -18,14 +17,26 @@ quotes_list = [
     """
     You need to avoid certain things in your train of thought: everything random,
     everything irrelevant. And certainly everything self-important or malicious.
+    """,
+    """
     You need to get used to winnowing your thoughts, so that if someone says,
     'What are you thinking?' you can respond at once (and truthfully) that you
     are thinking this or thinking that.
     """,
     """
+    An athlete in the greatest of all contests - the struggle not to be
+    overwhelmed by anything that happens.
+    """,
+    """
     He does only what is his to do, and considers constantly what the world has
     in store for him - doing his best, and trusting that all is for the best.
     For we carry our fate with us - and it carries us.
+    """,
+    """
+    He keeps in mind that all rational things are related, and that to care for
+    all human beings is part of being human. Which doesn't mean we have to share
+    their opinions. We should only listen only to those whose lives conform to
+    nature.
     """,
     """
     How to act 1: Never under compulsion, out of selfishness, without
@@ -48,6 +59,12 @@ quotes_list = [
     """,
     """
     How to act 6: To stand up straight - not straigthened.
+    """,
+    """
+    It would be wrong for anything to stand between you and attaining goodness.
+    Anything at all: the applause of the crowd, high office, wealth, or
+    self-indulgence. All of them might seem to be compatible with it, but
+    suddenly they control us and sweep us away.
     """,
     """
     Never regard something as doing you good if it makes you betray a trust, or
@@ -73,8 +90,7 @@ quotes_list = [
     Nothing is so conducive to spiritual growth as this capacity for logical and
     accurate analysis of everything that happens to us. To look at it in such a
     way that we understand what need it fulfills, and in what kind of world. And
-    its value to that world as a whole and to man in particular - as a citizen
-    of that higher city, of which all other cities are mere households.
+    its value to that world as a whole and to man in particular.
     """,
     """
     If you do the job in a principled way, with diligence, energy and patience,
@@ -88,9 +104,18 @@ quotes_list = [
     (every word, every utterance) - then your life will be happy.
     """,
     """
+    Doctors keep their scalpels and other instruments handy, for emergencies.
+    Keep your philosophy ready too - ready to understand heaven and earth.
+    """,
+    """
     In everything you do, even the smallest thing, remember the chain that links
     them. Nothing earthly succeeds by ignoring heaven, nothing heavenly by
     ignoring the earth.
+    """,
+    """
+    Stop drifting. You're not going to re-read your Brief Comments, your Deeds
+    of the Ancient Greeks and Romans, the commonplace books you saved for your
+    old age.
     """,
     """
     Sprint for the finish. Write off your hopes, and if your well being matters
