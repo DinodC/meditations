84fa837b33e7b8c625e04a83b4611f9fc6aef01f
dino <dino.decastro@gmail.com>
2019-02-24 19:20:12 +0800

    Cleaned book 1

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
     # book_to_import = import_module('static.quotes.' + temp_bookname)
 
     # For testing, fix book number
-    book_to_import = import_module('static.quotes.book_' + '12')
+    book_to_import = import_module('static.quotes.book_' + '01')
 
 
     # Pick a quote
@@ -30,7 +30,7 @@ def index():
     temp_quote = random.choice(temp_quote_list)
 
     # For testing, fix quote index
-    temp_quote = temp_quote_list[33]
+    temp_quote = temp_quote_list[31]
 
 
     #  Pick an image
