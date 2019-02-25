44ee03b25a853a1d2f9ede31a3123aea38c1259b
dino <dino.decastro@gmail.com>
2019-02-25 14:22:16 +0800

    Updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask import render_template, url_for
 
-#
 import os
 import random
 
-#
 import sys
 from importlib import import_module
 
@@ -19,27 +17,26 @@ def index():
     while temp_bookname.split('_')[0] != 'book':
         temp_book = random.choice(os.listdir('static/quotes/'))
         temp_bookname = os.path.splitext(temp_book)[0]
-    # book_to_import = import_module('static.quotes.' + temp_bookname)
 
-    # For testing, fix book number
-    book_to_import = import_module('static.quotes.book_' + '05')
+    # For production: pick a book
+    book_to_import = import_module('static.quotes.' + temp_bookname)
+    # # For testing: fix a book
+    # book_to_import = import_module('static.quotes.book_' + '05')
 
 
-    # Pick a quote
+    # For production: pick a quote
     temp_quote_list = book_to_import.quotes_list
     temp_quote = random.choice(temp_quote_list)
+    # # For testing: fix quote index
+    # temp_quote = temp_quote_list[36]
 
-    # For testing, fix quote index
-    temp_quote = temp_quote_list[36]
 
-
-    #  Pick an image
+    # For production: pick an image
     temp_image = random.choice(os.listdir('static/images/'))
     while temp_image.endswith('.DS_Store'):
         temp_image = random.choice(os.listdir('static/images/'))
     temp_image = 'images/' + temp_image
-
-    # For testing
+    # # For testing: fix image
     # temp_image = 'images/marcus_aurelius_noir.jpeg'
 
     return render_template("index.html", quote=temp_quote, image=temp_image)
